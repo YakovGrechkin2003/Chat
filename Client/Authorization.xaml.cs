50e8f444ad44ba9fae0dcc7fc78fad55a2f79d1d
YakovGrechkin2003 <58858243+YakovGrechkin2003@users.noreply.github.com>
2020-12-04 14:57:17 +0300

    uploaded the project

---
+++ Client/Authorization.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_next_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TextBox_Name.Text != "")
+                {
+                    string path = @"D:\All\ClientName.txt";
+                    string temp;
+                    using (StreamWriter sw = new StreamWriter(path))
+                    {
+                        temp = TextBox_Name.Text.ToString();
+                        sw.WriteLine(temp);
+                    }
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Введите имя!!");
+                }
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+        }
+    }
+}
---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //client.Connect(host, port);
+            //stream = client.GetStream();
+             string userName;
+             const string host = "127.0.0.1";
+             const int port = 8888;
+             TcpClient client;
+             NetworkStream stream;
+        }
+
+        Authorization autorization = new Authorization();
+        Thread listenThread;
+
+        //----------------------------------------------------------------------------
+        private void UserNameGet()
+        {
+
+        }
+        //----------------------------------------------------------------------------
+        private void Button_Autoriz_Click(object sender, RoutedEventArgs e)
+        {
+            autorization.Show();
+            Button_Autoriz.IsEnabled = false;
+        }
+    }
+}
